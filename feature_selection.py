e6e7ae256b58dea44d702e15aecd7a807999cddb
Foresti Matteo <matteo.foresti@cerved.com>
2023-08-02 17:58:30 +0200

    Minor Chnages

--- feature_selection.py
+++ feature_selection.py
@@ -4,19 +4,19 @@ from sklearn.feature_selection import f_classif
 from itertools import compress
 from scipy.stats import chi2_contingency
 
-def feature_selection_phi(data, kpi, threshold_phi=0.05):
+def chi2_table(data, target, threshold_phi=0.05):
     selected_features = []
     for feature in data.columns:
-        if feature == kpi:
+        if feature == target:
             continue
-        contingency_table = pd.crosstab(data[kpi], data[feature])
+        contingency_table = pd.crosstab(data[target], data[feature])
         chi2, p, dof, expected = chi2_contingency(contingency_table)
         phi = np.sqrt(chi2 / (data.shape[0] * (min(contingency_table.shape) - 1)))
         if phi >= threshold_phi:
             selected_features.append(feature)     
     return selected_features
 
-def driver_selection_cont(data_set, kpi, threshold_r2, threshold_phi, p_value, drivers):
+def driver_selection(data_set, target, threshold_r2, threshold_phi, p_value, drivers):
     num_drivers = []
     cat_drivers = []
     for driver in drivers:
@@ -26,15 +26,15 @@ def driver_selection_cont(data_set, kpi, threshold_r2, threshold_phi, p_value, d
             cat_drivers.append(driver)
 
     #the target is continous        
-    if data_set[kpi].nunique() != 2:
+    if data_set[target].nunique() != 2:
         #for continous variables -> Pearson correlation  
-        drivers_kpi = [kpi] + num_drivers
+        drivers_kpi = [target] + num_drivers
         corr_matrix = data_set[drivers_kpi].corr()[0:1].iloc[:, 1:]
         final_drivers_cont = [column for column in corr_matrix.columns if any(abs(corr_matrix[column]) >= threshold_r2)]
-        #for categorical variables -> ANOVA
+        #for binary/categorical variables -> ANOVA
         data_set.dropna(subset=cat_drivers, inplace=True)
         X = data_set[cat_drivers]
-        y = data_set[kpi]
+        y = data_set[target]
         final_drivers_cat = []
         if X.empty: 
             pass
@@ -43,13 +43,13 @@ def driver_selection_cont(data_set, kpi, threshold_r2, threshold_phi, p_value, d
             final_drivers_cat = list(compress(data_set[cat_drivers].columns.tolist(), pvalues<=p_value))
     #the target is binary
     else:
-        #for categorical variables -> Phi co-efficient
-        drivers_kpi = [kpi] + cat_drivers
-        final_drivers_cat = feature_selection_phi(data_set[drivers_kpi], kpi, threshold_phi)
+        #for binary/categorical variables -> Phi co-efficient
+        drivers_kpi = [target] + cat_drivers
+        final_drivers_cat = chi2_table(data_set[drivers_kpi], target, threshold_phi)
         #for continous variables -> ANOVA
         data_set.dropna(subset=num_drivers, inplace=True)
         X = data_set[num_drivers]
-        y = data_set[kpi]
+        y = data_set[target]
         final_drivers_cont = [] 
         if X.empty: 
             pass
@@ -65,7 +65,7 @@ def identify_multicollinearity(dataframe, threshold=0.75):
     corr_matrix = dataframe.corr().abs()
     np.fill_diagonal(corr_matrix.values, 0)
     
-    # Find features with correlation above the threshold
+    #find features with correlation above the threshold
     correlated_features = np.where(corr_matrix > threshold)
     correlated_features = [(corr_matrix.index[x], corr_matrix.columns[y]) for x, y in zip(*correlated_features) if x != y and x < y]
 
